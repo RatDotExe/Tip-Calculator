9cddd379f20bb887706f3027542599747d4dda52
Zachary <rat@ratdotexe.com>
2013-04-01 13:55:42 -0400

    First Commit

---
+++ Tip Calculator.cpp
@@ -0,0 +1,33 @@
+//
+// Rat_Dot_Exe's Tip Calculator
+// Written in C++
+//
+
+
+#include <iostream>
+#include <stdio.h>
+#include <string>
+
+using namespace std;
+int main() {
+	
+	double t;
+	double p;
+	double r;
+	double d;
+	int hund = 100;
+	
+	cout << "What's the total price?\n";
+	cin >> t;
+	
+	cout << "What's the %?\n";
+	cin >> p;
+	
+	d = p / hund;
+	
+	r = t * d;
+	
+	cout << "Result: " << r;
+	
+	
+}
